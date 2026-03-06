5da9a5173584fce9488c592024180efc0f6fef8a
HumanStewie <mopro1010@gmail.com>
2026-03-07 02:54:10 +0700

    feat: A working renderer! We need to fix a few stuffs like FPS, how to connect shape, make it more modular. But all the stuff we need is here!

--- src/Renderer.cpp
+++ src/Renderer.cpp
@@ -15,7 +15,6 @@ void Renderer::draw()
     // using clock = std::chrono::steady_clock;
     // auto duration { 1 / m_fps };
     // auto start { clock::now() };
-    std::cout << "\x1b[2J\x1b[1;1H";
     for (std::size_t i {}; i <= m_length; ++i) {
         // check if current char is the final char in the line or not
         // very clever trick used in Donut in C
@@ -36,14 +35,20 @@ void Renderer::draw()
 
 void Renderer::framebuffer(double A)
 {
-    for (double x {-2}; x <= 2; x += 0.5) {
-        double y { std::sqrt(4 - std::pow(x, 2)) };
-        Vector3D vec { x, y, 0.3 };
+    for (double x { -3 }; x <= 3; x += 0.01) {
+        double sinZ { sin(A) };
+        double cosZ { cos(A) };
+        double y { std::sqrt(9 - std::pow(x, 2)) };
+        Vector3D vec { x*cosZ , y, cosZ };
         double ooz { 1 / vec.z };
 
-        Vector3D projV { 40 + vec.x * ooz, 10 + vec.y * ooz, vec.z };
-        int out { static_cast<int>(projV.x + m_width * projV.y) }; // wrong type
-        m_fb[out] = '#';
+        Vector3D projV { 40 + 2.2 * vec.x * ooz, 10 + vec.y * ooz, vec.z };
+        int out { static_cast<int>(projV.x) +
+                  m_width * static_cast<int>(projV.y) };
+        if (projV.x > 0 && projV.y > 0 && vec.z > 0.2) {
+            m_fb[out]        = '#';
+            m_fb[1599 - out] = '#';
+        }
     }
 }
 
@@ -55,8 +60,13 @@ void Renderer::clear()
 
 void Renderer::render()
 {
-    double A { 5 };
-    clear();
-    framebuffer(A);
-    draw();
+    double A {};
+    while (true) {
+        std::cout << "\x1b[2J\x1b[1;1H";
+
+        clear();
+        framebuffer(A);
+        draw();
+        A += 0.04;
+    }
 }
